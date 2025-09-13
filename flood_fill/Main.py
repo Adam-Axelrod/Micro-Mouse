9a2a9d7fdc7bb19d623089f0b94ce3ca19d0c752
Adam Axelrod <adambaxelrod@gmail.com>
2025-09-13 18:29:36 +0100

    flood fill and left hug

--- flood_fill/Main.py
+++ flood_fill/Main.py
@@ -5,16 +5,20 @@ def log(string):
     sys.stderr.write("{}\n".format(string))
     sys.stderr.flush()
 
+
+
+
 def main():
     log("Running...")
     API.setColor(0, 0, "G")
     API.setText(0, 0, "abc")
     while True:
-        if not API.wallLeft():
-            API.turnLeft()
-        while API.wallFront():
-            API.turnRight()
-        API.moveForward()
+        maze.get_path()
+        mouse.check_walls()
+        if blocked:
+            maze.calculate_path()
+            break
+        mouse.move_along_path()
 
 if __name__ == "__main__":
     main()
